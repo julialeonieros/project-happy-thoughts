f92b561aee2bd0943b613f9c10f8eb9eaf843a86
Julia Ros <70485921+julialeonieros@users.noreply.github.com>
2021-05-19 22:24:49 +0200

    Added URL to my own database

--- code/src/utils/urls.js
+++ code/src/utils/urls.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://happy-thoughts-technigo.herokuapp.com/thoughts'
-export const LIKES_URL = (messageID) => `https://happy-thoughts-technigo.herokuapp.com/thoughts/${messageID}/like`
+export const API_URL = 'https://super-happy-thoughts.herokuapp.com/thoughts'
+export const LIKES_URL = (ID) => `https://super-happy-thoughts.herokuapp.com/thoughts/${ID}/like`
